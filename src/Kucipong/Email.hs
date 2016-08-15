24fcf7b2d1646187c5552c91e71617f1f6b8fe8d
(cdep)illabout <cdep.illabout@gmail.com>
2016-08-16 03:16:21 +0900

    Add small example showing how to send email.

--- src/Kucipong/Email.hs
+++ src/Kucipong/Email.hs
@@ -4,7 +4,55 @@ module Kucipong.Email where
 import Kucipong.Prelude
 
 import Mail.Hailgun
+    ( HailgunContext, HailgunErrorMessage, HailgunErrorResponse(..)
+    , HailgunMessage, HailgunSendResponse, MessageContent(..)
+    , MessageRecipients(..), emptyMessageRecipients, hailgunMessage, sendEmail )
+import Text.Email.Validate ( toByteString )
 
--- sendRegistrationCompletedEmail :: MonadIO m => m ()
--- sendRegistrationCompletedEmail =
---     sendEmail
+import Kucipong.Errors ( AppErr, AppErrEnum(..), throwAppErr )
+
+class HasHailgunContext r where
+    getHailgunContext :: r -> HailgunContext
+
+instance HasHailgunContext HailgunContext where
+    getHailgunContext = id
+
+sendRegistrationCompletedEmail
+    :: forall r m
+     . ( HasHailgunContext r
+       , MonadError AppErr m
+       , MonadIO m
+       , MonadReader r m
+       )
+    => EmailAddress -> m HailgunSendResponse
+sendRegistrationCompletedEmail companyEmail = do
+    hailgunContext <- reader getHailgunContext
+    either handleMessageError (trySendEmail hailgunContext) $
+        registrationCompleteMessage companyEmail
+  where
+    handleMessageError :: String -> m a
+    handleMessageError = throwAppErr OtherException . Just . pack
+
+    handleSendError :: HailgunErrorResponse -> m a
+    handleSendError = throwAppErr HailgunError . Just . pack . herMessage
+
+    trySendEmail :: HailgunContext -> HailgunMessage -> m HailgunSendResponse
+    trySendEmail hailgunContext =
+        either handleSendError pure <=< sendEmail' hailgunContext
+
+registrationCompleteMessage :: EmailAddress -> Either HailgunErrorMessage HailgunMessage
+registrationCompleteMessage companyEmail =
+    let subject = "test subject"
+        content = TextOnly "test content"
+        replyTo = "info@kucipong.com"
+        to = toByteString companyEmail
+        recipients = emptyMessageRecipients { recipientsTo = [ to ] }
+        attachements = []
+    in hailgunMessage subject content replyTo recipients attachements
+
+sendEmail'
+    :: MonadIO m
+    => HailgunContext
+    -> HailgunMessage
+    -> m (Either HailgunErrorResponse HailgunSendResponse)
+sendEmail' = (liftIO .) . sendEmail
--- src/Kucipong/Handler.hs
+++ src/Kucipong/Handler.hs
@@ -8,6 +8,11 @@ import Web.Spock ( Path, (<//>), get, html, root, runSpock, spockT, text, var )
 import Kucipong.Config ( Config, HasPort(..) )
 import Kucipong.Monad ( KucipongM, runKucipongM )
 
+-- TODO: Remove this:
+import Kucipong.Email
+import Mail.Hailgun
+import Text.Email.Validate (emailAddress)
+
 helloR :: Path '[Text]
 helloR = "hello" <//> var
 
@@ -24,4 +29,12 @@ app config = runSpock (getPort config) $
             -- dbLoginUser undefined undefined
             html "<p>hello world</p>"
         get helloR $ \name -> text $ "Hello " <> name <> "!"
-        get addR $ \a b -> text $ pack $ show (a + b)
+        get addR $ \a b -> text . pack $ show (a + b)
+        -- TODO: Remove this.  This is just an example of how to send email.
+        get "sendemail" $ do
+            res <- liftIO $ runKucipongM config $ do
+                mailgunContext <- reader getHailgunContext
+                print mailgunContext
+                resp <- sendRegistrationCompletedEmail (fromMaybe undefined $ emailAddress "kucipong.dev@gmail.com")
+                print resp
+            print res
