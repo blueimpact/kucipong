0f3ba7003f45c0347595a3db6e2d1a403ccde6e7
(cdep)illabout <cdep.illabout@gmail.com>
2016-08-15 20:17:44 +0900

    Add hailgun package.

---
+++ src/Kucipong/Email.hs
@@ -0,0 +1,10 @@
+
+module Kucipong.Email where
+
+import Kucipong.Prelude
+
+import Mail.Hailgun
+
+-- sendRegistrationCompletedEmail :: MonadIO m => m ()
+-- sendRegistrationCompletedEmail =
+--     sendEmail
