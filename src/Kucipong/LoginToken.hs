90672e9b92e6e5e2035447fdf28eb2ed5e2e7808
(cdep)illabout <cdep.illabout@gmail.com>
2016-08-29 14:44:14 +0900

    Make sure that the LoginToken is base64-encoded.

--- src/Kucipong/LoginToken.hs
+++ src/Kucipong/LoginToken.hs
@@ -4,6 +4,7 @@ module Kucipong.LoginToken where
 import Kucipong.Prelude
 
 import Control.Monad.Random ( MonadRandom, getRandoms )
+import Data.ByteString.Base64 ( encode )
 import Database.Persist ( PersistField(..) )
 import Database.Persist.Sql ( PersistFieldSql(..) )
 
@@ -11,4 +12,5 @@ newtype LoginToken = LoginToken { unLoginToken :: Text }
     deriving (Data, Eq, Generic, PersistField, PersistFieldSql, Show, Typeable)
 
 createRandomLoginToken :: MonadRandom m => m LoginToken
-createRandomLoginToken = LoginToken . pack . take 50 <$> getRandoms
+createRandomLoginToken =
+    LoginToken . decodeUtf8 . encode . pack . take 50 <$> getRandoms
