8ea38db6ac4834428701d327c229d0510651e791
(cdep)illabout <cdep.illabout@gmail.com>
2016-09-05 15:25:47 +0900

    Add function to Kucipong Cookie monad for getting the "secure" setting for cookie based on whether we are in the development or production environment.

--- src/Kucipong/Monad/Cookie/Class.hs
+++ src/Kucipong/Monad/Cookie/Class.hs
@@ -5,7 +5,7 @@ module Kucipong.Monad.Cookie.Class where
 import Kucipong.Prelude
 
 import Control.Monad.Trans ( MonadTrans )
-import Web.Spock ( ActionCtxT )
+import Web.Spock ( ActionCtxT, CookieSettings )
 
 import Kucipong.Monad.Db.Trans ( KucipongDbT )
 import Kucipong.Monad.SendEmail.Trans ( KucipongSendEmailT )
@@ -15,6 +15,17 @@ import Kucipong.Session ( Session )
 -- Default implementations are used to easily derive instances for monads
 -- transformers that implement 'MonadTrans'.
 class Monad m => MonadKucipongCookie m where
+    cookieSettings
+        :: m CookieSettings
+    default cookieSettings
+        :: ( Monad (t n)
+           , MonadKucipongCookie n
+           , MonadTrans t
+           , m ~ t n
+           )
+        => t n CookieSettings
+    cookieSettings = lift cookieSettings
+
     encryptSessionCookie
         :: Session sessionType
         -> m Text
--- src/Kucipong/Monad/Cookie/Instance.hs
+++ src/Kucipong/Monad/Cookie/Instance.hs
@@ -5,15 +5,37 @@ module Kucipong.Monad.Cookie.Instance where
 
 import Kucipong.Prelude
 
+import Web.Spock
+    ( CookieSettings(..), CookieEOL(..), defaultCookieSettings )
+
+import Kucipong.Environment ( Environment(Production), HasEnv(getEnv) )
 import Kucipong.Monad.Cookie.Class ( MonadKucipongCookie(..) )
 import Kucipong.Monad.Cookie.Trans ( KucipongCookieT(..) )
 import Kucipong.Session ( HasSessionKey, Session, encryptSession )
+import Kucipong.Util ( oneYear )
 
 instance
-    ( HasSessionKey r
+    ( HasEnv r
+    , HasSessionKey r
     , MonadIO m
     , MonadReader r m
     ) => MonadKucipongCookie (KucipongCookieT m) where
+
+    cookieSettings :: KucipongCookieT m CookieSettings
+    cookieSettings = do
+        env <- reader getEnv
+        case env of
+            Production -> pure develCookieSettings { cs_secure = True }
+            _ -> pure develCookieSettings
+      where
+        develCookieSettings :: CookieSettings
+        develCookieSettings = defaultCookieSettings
+            { cs_EOL = CookieValidFor oneYear
+            , cs_HTTPOnly = True
+            , cs_secure = False
+            , cs_domain = Nothing
+            , cs_path = Just "/"
+            }
 
     encryptSessionCookie :: Session sessionType -> KucipongCookieT m Text
     encryptSessionCookie = lift . encryptSession
