45a8d4ac54871ae2b35bcbdd6d0e941da7efbf57
Kadzuya Okamoto <arow.okamoto@gmail.com>
2016-10-09 14:00:05 +0900

    Simple implementation of MonadKucipongDb instances for store user

--- src/Kucipong/Monad/Db/Instance.hs
+++ src/Kucipong/Monad/Db/Instance.hs
@@ -14,8 +14,8 @@ import Database.Persist
 import Kucipong.Config ( Config )
 import Kucipong.Db
     ( Admin(..), AdminLoginToken(..), CreatedTime(..), EntityField(..), Key(..)
-    , LoginTokenExpirationTime(..), UpdatedTime(..), adminName, runDb
-    , runDbCurrTime )
+    , LoginTokenExpirationTime(..), Store(..), StoreLoginToken(..)
+    , UpdatedTime(..), adminName, runDb, runDbCurrTime, storeName )
 import Kucipong.Errors ( AppErr )
 import Kucipong.LoginToken ( LoginToken, createRandomLoginToken )
 import Kucipong.Monad.Db.Class ( MonadKucipongDb(..) )
@@ -30,6 +30,9 @@ instance ( MonadBaseControl IO m
          , MonadTime m
          ) => MonadKucipongDb (KucipongDbT m) where
 
+    -- ===========
+    --  For Admin
+    -- ===========
     dbCreateAdmin :: EmailAddress -> Text -> KucipongDbT m (Entity Admin)
     dbCreateAdmin email name = lift go
       where
@@ -85,3 +88,66 @@ instance ( MonadBaseControl IO m
                             (UpdatedTime currTime) Nothing name
                     newAdminKey <- insert newAdminVal
                     pure $ Entity newAdminKey newAdminVal
+
+    -- ===========
+    --  For Store
+    -- ===========
+    dbCreateStore :: EmailAddress -> Text -> KucipongDbT m (Entity Store)
+    dbCreateStore email name = lift go
+      where
+        go :: m (Entity Store)
+        go = do
+            currTime <- currentTime
+            let store =
+                    Store email (CreatedTime currTime) (UpdatedTime currTime)
+                        Nothing name
+                        Nothing Nothing Nothing Nothing Nothing Nothing
+                        Nothing Nothing Nothing
+            storeKey <- runDb $ insert store
+            pure $ Entity storeKey store
+
+    dbCreateStoreMagicLoginToken :: Key Store -> KucipongDbT m (Entity StoreLoginToken)
+    dbCreateStoreMagicLoginToken storeKey = lift go
+      where
+        go :: m (Entity StoreLoginToken)
+        go = do
+            currTime <- currentTime
+            randomLoginToken <- createRandomLoginToken
+            let plusOneDay = addOneDay currTime
+            let newStoreLoginTokenVal =
+                    StoreLoginToken storeKey (CreatedTime currTime)
+                        (UpdatedTime currTime) Nothing randomLoginToken
+                        (LoginTokenExpirationTime plusOneDay)
+            runDb $ repsert (StoreLoginTokenKey storeKey) newStoreLoginTokenVal
+            pure $ Entity (StoreLoginTokenKey storeKey) newStoreLoginTokenVal
+
+    dbFindStoreLoginToken :: LoginToken -> KucipongDbT m (Maybe (Entity StoreLoginToken))
+    dbFindStoreLoginToken loginToken = lift go
+      where
+        go :: m (Maybe (Entity StoreLoginToken))
+        go = runDb $ selectFirst [StoreLoginTokenLoginToken ==. loginToken] []
+
+    dbUpsertStore :: EmailAddress -> Text -> KucipongDbT m (Entity Store)
+    dbUpsertStore email name = lift go
+      where
+        go :: m (Entity Store)
+        go = runDbCurrTime $ \currTime -> do
+            maybeExistingStoreVal <- get (StoreKey email)
+            case maybeExistingStoreVal of
+                Just existingStoreVal -> do
+                    -- store already exists.  update the name if it is different
+                    if (existingStoreVal ^. storeName /= name)
+                        then do
+                            newStoreVal <- updateGet (StoreKey email) [StoreName =. name]
+                            pure $ Entity (StoreKey email) newStoreVal
+                        else
+                            pure $ Entity (StoreKey email) existingStoreVal
+                Nothing -> do
+                    -- couldn't find an existing store, so we will create a new
+                    -- one
+                    let newStoreVal = Store email (CreatedTime currTime)
+                            (UpdatedTime currTime) Nothing name
+                            Nothing Nothing Nothing Nothing Nothing Nothing
+                            Nothing Nothing Nothing
+                    newStoreKey <- insert newStoreVal
+                    pure $ Entity newStoreKey newStoreVal
