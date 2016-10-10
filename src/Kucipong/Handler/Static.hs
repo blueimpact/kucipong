9d0de53de69729ccfc42f0c787d52d576fbb993a
Kadzuya Okamoto <arow.okamoto@gmail.com>
2016-10-10 19:27:12 +0900

    Set appropreate content type for static files

--- src/Kucipong/Handler/Static.hs
+++ src/Kucipong/Handler/Static.hs
@@ -7,8 +7,8 @@ import           Kucipong.Prelude
 import           Language.Haskell.TH        (Exp, Q)
 import           Language.Haskell.TH.Syntax (addDependentFile, runIO)
 import           System.FilePath.Glob       (glob)
-import           System.FilePath.Posix      (takeFileName)
-import           Web.Spock                  (bytes, static)
+import           System.FilePath.Posix      (takeExtension, takeFileName)
+import           Web.Spock                  (bytes, setHeader, static)
 import           Web.Spock.Core             (get)
 
 staticComponent' :: Q Exp
@@ -22,12 +22,24 @@ staticComponent' = do
     -- setHeader "Content-Type" "text/html; charset=utf-8"
     [e| forM_ fileContents $ \(fp, content) ->
         get (static $ takeFileName fp) $
+          let
+            contentType = case takeExtension fp of
+                ".css" -> Just "text/css; charset=utf-8"
+                ".gif" -> Just "image/gif"
+                ".js" -> Just "application/javascript; charset=utf-8"
+                ".jpeg" -> Just "image/jpeg"
+                ".jpg" -> Just "image/jpeg"
+                ".png" -> Just "image/png"
+                ".svg" -> Just "image/svg+xml; charset=utf-8"
+                _ -> Nothing
+          in do
+            maybe (pure ()) (setHeader "Content-Type") contentType
             bytes $ encodeUtf8 content |]
   where
     handleFileContent :: FilePath -> IO Text -> IO (Maybe (FilePath, Text))
     handleFileContent fp a =
         catch (a >>= \v -> return (Just (fp, v))) $ \e -> do
-            fail $
+            void . fail $
                 "exception occured when trying to parse the static file \"" <>
                 fp <> ":\n" <> show (e :: SomeException)
             pure Nothing
