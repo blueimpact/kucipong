238924ec2e5c7d9b70100575c468ceb5cba7bef1
(cdep)illabout <cdep.illabout@gmail.com>
2016-10-24 21:17:57 +0900

    Reformat Kucipong.Handler with hindent.

--- src/Kucipong/Handler.hs
+++ src/Kucipong/Handler.hs
@@ -4,34 +4,36 @@ module Kucipong.Handler where
 
 import Kucipong.Prelude
 
-import Data.HVect ( HVect(HNil) )
-import Network.Wai ( Middleware )
-import Web.Spock ( ActionCtxT, html , root, runSpock )
-import Web.Spock.Core ( spockT, get, prehook, subcomponent )
+import Data.HVect (HVect(HNil))
+import Network.Wai (Middleware)
+import Web.Spock (ActionCtxT, html, root, runSpock)
+import Web.Spock.Core (spockT, get, prehook, subcomponent)
 
-import Kucipong.Config ( Config )
-import Kucipong.Handler.Admin ( adminComponent, adminUrlPrefix )
-import Kucipong.Handler.Store ( storeComponent, storeUrlPrefix )
-import Kucipong.Handler.Static ( staticComponent, staticUrlPrefix )
-import Kucipong.Host ( HasPort(..) )
-import Kucipong.Monad ( KucipongM, runKucipongM )
+import Kucipong.Config (Config)
+import Kucipong.Handler.Admin (adminComponent, adminUrlPrefix)
+import Kucipong.Handler.Static (staticComponent, staticUrlPrefix)
+import Kucipong.Handler.Store (storeComponent, storeUrlPrefix)
+import Kucipong.Host (HasPort(..))
+import Kucipong.Monad (KucipongM, runKucipongM)
 
 runKucipongMHandleErrors :: Config -> KucipongM a -> IO a
 runKucipongMHandleErrors config = either throwIO pure <=< runKucipongM config
 
-baseHook :: Monad m => ActionCtxT () m (HVect '[])
+baseHook
+  :: Monad m
+  => ActionCtxT () m (HVect '[])
 baseHook = pure HNil
 
 app :: Middleware -> Config -> IO ()
 app middleware config = do
-    spockMiddleware <- spockMiddlewareIO
-    runSpock (getPort config) . pure $ middleware . spockMiddleware
+  spockMiddleware <- spockMiddlewareIO
+  runSpock (getPort config) . pure $ middleware . spockMiddleware
   where
     spockMiddlewareIO :: IO Middleware
     spockMiddlewareIO =
-        spockT (runKucipongMHandleErrors config) $ do
-            subcomponent staticUrlPrefix staticComponent
-            prehook baseHook $ do
-                subcomponent adminUrlPrefix adminComponent
-                subcomponent storeUrlPrefix storeComponent
-            get root $ do html "<p>hello world</p>"
+      spockT (runKucipongMHandleErrors config) $ do
+        subcomponent staticUrlPrefix staticComponent
+        prehook baseHook $ do
+          subcomponent adminUrlPrefix adminComponent
+          subcomponent storeUrlPrefix storeComponent
+        get root $ do html "<p>hello world</p>"
