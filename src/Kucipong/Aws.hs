537645d90f003ed8e8c5bee4a75aced003a7008f
(cdep)illabout <cdep.illabout@gmail.com>
2017-02-10 17:53:36 +0900

    Add in some notes.

--- src/Kucipong/Aws.hs
+++ src/Kucipong/Aws.hs
@@ -28,6 +28,7 @@ import Network.AWS.S3
         createBucketConfiguration)
 import Network.HTTP.Types (Status(Status, statusCode))
 
+-- | Newtype wrapper around the S3 image bucket name.
 newtype S3ImageBucketName = S3ImageBucketName
   { unS3ImageBucketName :: Text
   } deriving (Data, Eq, Generic, IsString, Ord, Show, Typeable)
@@ -46,6 +47,14 @@ instance HasAwsRegion Region where
   getAwsRegion :: Region -> Region
   getAwsRegion = id
 
+-- | Given a 'Region' and an 'Env', try to create a bucket on S3 with the name
+-- 'S3ImageBucketName'.
+--
+-- Succeed if either the bucket has been successfully created, or the bucket
+-- already exists.
+--
+-- Fail with 'error' if the bucket could not be created.  This could happen for
+-- many reasons.  For example, because of no network connectivity.
 createS3ImageBucket
   :: (MonadBaseControl IO m, MonadIO m, MonadLogger m, MonadThrow m)
   => Region -> Env -> S3ImageBucketName -> m ()
@@ -79,5 +88,6 @@ createS3ImageBucket awsRegion awsEnv s3ImageBucketName = do
         "\" on aws: " <>
         show errResp
 
+-- | Convert an 'S3ImageBucketName' to a 'BucketName'.
 s3ImageBucketNameToBucketName :: S3ImageBucketName -> BucketName
 s3ImageBucketNameToBucketName = BucketName . unS3ImageBucketName
