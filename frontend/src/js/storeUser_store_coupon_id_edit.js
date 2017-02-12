d5872aff8e4d3ef79dfee56af9f9ecdcfdeb839d
Kadzuya Okamoto <arow.okamoto@gmail.com>
2017-02-12 13:54:20 +0900

    Simplify with CSS pseudo-class.

--- frontend/src/js/storeUser_store_coupon_id_edit.js
+++ frontend/src/js/storeUser_store_coupon_id_edit.js
@@ -2,22 +2,11 @@
 
 require('date-input-polyfill');
 
-var showByCouponType = function () {
-  var selected = document.getElementById("couponType").value;
-  Array.prototype.slice.call(
-    document.getElementsByClassName("js-visible") || []
-  ).forEach(function (elem){
-    elem.classList.remove("js-visible");
-    elem.classList.add("js-invisible");
-  });
-  var toShow = {
-    "\#{couponTypeToText CouponTypeDiscount}": document.getElementById("js-couponDiscount"),
-    "\#{couponTypeToText CouponTypeGift}": document.getElementById("js-couponGift"),
-    "\#{couponTypeToText CouponTypeSet}": document.getElementById("js-couponSet"),
-    "\#{couponTypeToText CouponTypeOther}": document.getElementById("js-couponOther")
-  }[selected];
-  toShow.classList.remove("js-invisible");
-  toShow.classList.add("js-visible");
+var couponType = document.getElementById("couponType");
+var onChangeCouponType = function () {
+  window.setTimeout(function () {
+    window.location.replace('#type-' + couponType.value)
+  }, 200);
 };
-document.getElementById("couponType").addEventListener("change", showByCouponType, false);
-showByCouponType();
+couponType.addEventListener('change', onChangeCouponType);
+onChangeCouponType();
