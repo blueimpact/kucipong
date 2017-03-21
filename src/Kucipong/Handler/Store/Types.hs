57557483fdb0b1280a7bb7bcd9c5482ecfc43320
Kadzuya Okamoto <arow.okamoto@gmail.com>
2017-03-21 14:17:12 +0900

    Declare data types representing View.

--- src/Kucipong/Handler/Store/Types.hs
+++ src/Kucipong/Handler/Store/Types.hs
@@ -1,10 +1,20 @@
 module Kucipong.Handler.Store.Types
   ( StoreError(..)
   , StoreMsg(..)
+  , CouponView(..)
+  , CouponViewKey(..)
+  , CouponViewTypes(..)
+  , CouponViewConditions(..)
+  , CouponViewCouponType(..)
   ) where
 
 import Kucipong.Prelude
 
+import Database.Persist (Entity(..))
+
+import Kucipong.Db (Coupon(..), Store(..))
+
+-- For I18n.
 data StoreError
   = StoreErrorBusinessCategoryDetailIncorrect
   | StoreErrorCouldNotSendEmail
@@ -14,7 +24,43 @@ data StoreError
   | StoreErrorNotAnImage
   deriving (Show, Eq, Ord, Read)
 
-data StoreMsg
-  = StoreMsgSentVerificationEmail
+data StoreMsg =
+  StoreMsgSentVerificationEmail
   deriving (Show, Eq, Ord, Read, Enum, Bounded)
 
+-- For View.
+data CouponView = CouponView
+  { couponStore :: Entity Store
+  , couponCoupon :: Entity Coupon
+  , couponImageUrl :: Maybe Text
+  } deriving (Show, Eq)
+
+data CouponViewKey
+  = StoreId
+  | CouponId
+
+data CouponViewTypes
+  = StoreName
+  | StoreAddress
+  | ImageUrl
+  | Title
+  | ValidFrom
+  | ValidUntil
+  | DiscountPercent
+  | DiscountMinimumPrice
+  | GiftContent
+  | GiftMinimumPrice
+  | GiftReferencePrice
+  | SetContent
+  | SetPrice
+  | SetReferencePrice
+  | OtherContent
+
+data CouponViewConditions
+  = DiscountOtherConditions
+  | GiftOtherConditions
+  | SetOtherConditions
+  | OtherConditions
+
+data CouponViewCouponType =
+  CouponType
